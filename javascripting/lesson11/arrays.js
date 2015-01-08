b4c7d0701677d751fe9d3896e2bf8a719e5de4d4
MoFreeman <mofreeman@gmail.com>
2015-01-08 16:50:30 -0500

    javascripting lesson 11

---
+++ javascripting/lesson11/arrays.js
@@ -0,0 +1,9 @@
+pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+console.log(pizzaToppings);
+
+//console.log(pizzaToppings.toString());
+
+//for (var i = 0; i < pizzaToppings.length; i++)
+//{
+//  console.log(pizzaToppings[i]);
+//}
