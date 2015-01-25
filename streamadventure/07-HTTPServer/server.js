73ca5bac9763ce0a107519b7f085d293280e7cbc
mofreeman <mofreeman@gmail.com>
2015-01-25 03:57:53 +0000

    stream adventure lesson  07 - http server

---
+++ streamadventure/07-HTTPServer/server.js
@@ -0,0 +1,15 @@
+    var http = require('http');
+    var through = require('through');
+    
+    var server = http.createServer(function (req, res) {
+        if (req.method === 'POST') {
+            req.pipe(through(write)).pipe(res);
+        }
+    });
+    server.listen(process.argv[2]);
+    
+    
+    function write (buf)
+    {
+        this.queue(buf.toString().toUpperCase());
+    };
